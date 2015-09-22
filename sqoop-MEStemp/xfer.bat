b092f12b3bc7974ee835a65aecc200faf9afcdd4
chrisgerken <chris.gerken@gerkenip.com>
2015-09-22 13:00:10 -0500

    correct user

--- sqoop-MEStemp/xfer.bat
+++ sqoop-MEStemp/xfer.bat
@@ -1,6 +1,6 @@
-pscp  -pw   SqoopWorkflow.xml  app_MES_ReadOnlyUserMX@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/SqoopWorkflow.xml
-pscp  -pw   JobProperties.xml  app_MES_ReadOnlyUserMX@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/JobProperties.xml
-pscp  -pw   runSqoop.sh        app_MES_ReadOnlyUserMX@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/runScoop.sh
+pscp  -pw   SqoopWorkflow.xml  appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/SqoopWorkflow.xml
+pscp  -pw   JobProperties.xml  appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/JobProperties.xml
+pscp  -pw   runSqoop.sh        appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/runScoop.sh
 
-pscp  -pw   CreateTable.txt    app_MES_ReadOnlyUserMX@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/CreateTable.txt
-pscp  -pw   runHive.sh         app_MES_ReadOnlyUserMX@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/runHive.sh
+pscp  -pw   CreateTable.txt    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/CreateTable.txt
+pscp  -pw   runHive.sh         appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/runHive.sh
