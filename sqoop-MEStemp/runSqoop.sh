bc1be0f55532f9a3c40125de1fe78e4f11989c0a
chrisgerken <chris.gerken@gerkenip.com>
2015-09-24 13:47:33 -0500

    prop

--- sqoop-MEStemp/runSqoop.sh
+++ sqoop-MEStemp/runSqoop.sh
@@ -1,3 +1,3 @@
 hadoop fs -put yieldIngestWorkflow.xml /MEStemp/yieldIngestWorkflow.xml
  
-oozie  job  -oozie  http://prod-hubnamedaws01.sunpowercorp.com:11000/oozie  -config  yieldIngestProperties.txt  -run
+oozie  job  -oozie  http://prod-hubnamedaws01.sunpowercorp.com:11000/oozie  -config  yieldIngestProperties.properties  -run
