93bd5f7e3aa29afe8e1f6ee914a4e800f859ab45
chrisgerken <chris.gerken@gerkenip.com>
2015-09-24 11:06:11 -0500

    oozie

--- sqoop-MEStemp/stage2.sh
+++ sqoop-MEStemp/stage2.sh
@@ -1,2 +1,5 @@
 sudo cp CreateTable.txt  /home/hdfs/CreateTable.txt
 sudo cp runHive.sh  /home/hdfs/runHive.sh
+sudo cp wipeDB.sh  /home/hdfs/wipeDB.sh
+sudo cp yieldIngestWorkflow.xml  /home/hdfs/yieldIngestWorkflow.xml
+sudo cp yieldIngestProperties.xml  /home/hdfs/yieldIngestProperties.xml
