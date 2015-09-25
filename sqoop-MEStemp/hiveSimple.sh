4e38dd639751996c8634bf1ea2b978356cb2e7c8
chrisgerken <chris.gerken@gerkenip.com>
2015-09-25 15:48:42 -0500

    furq2

--- sqoop-MEStemp/hiveSimple.sh
+++ sqoop-MEStemp/hiveSimple.sh
@@ -12,7 +12,9 @@ hadoop fs -test -d /MEStemp
                  echo "/MEStemp not present in HDFS"
     fi
 
-sqoop import --connect 'jdbc:sqlserver://172.23.236.90:1433;database=MEXICALI_OWNER' --username app_MES_ReadOnlyUserMX --password X2{01_p7gyltm --query "SELECT BuildRecordID,InvPackCreatedID,InvPackConsumedID,QuantityUsed,UnitID,BuildDT,IsEdited,RevisionNo,BuildMachineStageID, 1 as \"org_id\"  FROM dbo.BuildRecord WHERE \$CONDITIONS" --split-by BuildRecordID  -m 15 --target-dir /MEStemp/BuildRecord/1 --direct  -- --schema dbo --validate
+sqoop import --connect 'jdbc:sqlserver://172.23.236.90:1433;database=MEXICALI_OWNER' --username app_MES_ReadOnlyUserMX --password X2{01_p7gyltm --query "SELECT BuildRecordID,InvPackCreatedID,InvPackConsumedID,QuantityUsed,UnitID,BuildDT,IsEdited,RevisionNo,BuildMachineStageID, 1 as \"org_id\"  FROM dbo.BuildRecord WHERE \$CONDITIONS" --split-by BuildRecordID  --num-mappers 2 --target-dir /MEStemp/BuildRecord/1 --direct  -- --schema dbo --validate
+
+sqoop import --connect 'jdbc:sqlserver://172.23.236.90:1433;database=MEXICALI_OWNER' --username app_MES_ReadOnlyUserMX --password X2{01_p7gyltm --query "SELECT AddressID,Address1,Address2,City,County,PostCode,Country, 1 as \"org_id\"  FROM dbo.Address WHERE \$CONDITIONS" --split-by AddressID  --num-mappers 2 --target-dir /MEStemp/Address/1 --direct  -- --schema dbo --validate
 
 
 set hive.execution.engine=tez;
@@ -22,10 +24,11 @@ hive
 CREATE DATABASE IF NOT EXISTS MEStemp;
 
 
-Create External table IF NOT EXISTS MEStemp.Asset (AssetID int, AssetName varchar(508), RfAssetTypeID smallint, LineID smallint, MachineStageID smallint, ToolID int, GaugeID smallint, PartID int, InventoryPackID int, LocationID int, IsRetired tinyint, CostCentreID smallint, ManufacturerID smallint, SupplierID smallint, AcquiredDT timestamp, PONumber varchar(100), OrderedDT timestamp, ExpectedDeliveryDT timestamp, ExpectedFirstUseDT timestamp, EndUseDT timestamp, EndUseReason varchar(100), PurchaseCost float, DefaultLocationID int, Comments varchar(508),  org_id int) row format delimited fields terminated by ',' lines terminated by '\n' stored as textfile location '/MEStemp/Asset/';
-
-
 Create External table IF NOT EXISTS MEStemp.BuildRecord (BuildRecordID int, InvPackCreatedID int, InvPackConsumedID int, QuantityUsed float, UnitID smallint, BuildDT timestamp, IsEdited tinyint, RevisionNo smallint, BuildMachineStageID smallint,  org_id int) row format delimited fields terminated by ',' lines terminated by '\n' stored as textfile location '/MEStemp/BuildRecord/';
 
 
+Create External table IF NOT EXISTS MEStemp.Address (AddressID smallint, Address1 varchar(508), Address2 varchar(508), City varchar(100), County varchar(100), PostCode varchar(100), Country varchar(100),  org_id int) row format delimited fields terminated by ',' lines terminated by '\n' stored as textfile location '/MEStemp/Address/';
 
+
+
+quit;
--- sqoop-MEStemp/wipeDB.sh
+++ sqoop-MEStemp/wipeDB.sh
@@ -1,16 +1,5 @@
 
 
-hive drop table MEStemp/Asset;
-
-hadoop fs -test -d /MES/PartType
-    if [ $? = 0 ]
-            then
-                hdfs dfs -rm -r /MEStemp/Asset
-                else
-                 echo "/MEStemp/Asset not present in HDFS"
-    fi
-
-
 hive drop table MEStemp/BuildRecord;
 
 hadoop fs -test -d /MES/PartType
@@ -19,6 +8,17 @@ hadoop fs -test -d /MES/PartType
                 hdfs dfs -rm -r /MEStemp/BuildRecord
                 else
                  echo "/MEStemp/BuildRecord not present in HDFS"
+    fi
+
+
+hive drop table MEStemp/Address;
+
+hadoop fs -test -d /MES/PartType
+    if [ $? = 0 ]
+            then
+                hdfs dfs -rm -r /MEStemp/Address
+                else
+                 echo "/MEStemp/Address not present in HDFS"
     fi
 
     
