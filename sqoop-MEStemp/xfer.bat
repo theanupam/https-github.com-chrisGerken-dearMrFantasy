d326a85734e1ee4145853bf9307ea26bc953b69a
chrisgerken <chris.gerken@gerkenip.com>
2015-09-25 14:56:07 -0500

    spl1

--- sqoop-MEStemp/xfer.bat
+++ sqoop-MEStemp/xfer.bat
@@ -4,7 +4,7 @@ pscp  -pw d4e30f24   runSqoop.sh        appladm@prod-hubnamedaws01.sunpowercorp.
 
 pscp  -pw d4e30f24   CreateTable.txt    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/CreateTable.txt
 pscp  -pw d4e30f24   runHive.sh         appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/runHive.sh
+pscp  -pw d4e30f24   hiveSimple.sh         appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/hiveSimple.sh
 
 pscp  -pw d4e30f24   stage2.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/stage2.sh
-pscp  -pw d4e30f24   HiveSimple.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/HiveSimple.sh
 pscp  -pw d4e30f24   wipeDB.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/wipeDB.sh
