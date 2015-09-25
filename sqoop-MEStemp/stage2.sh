83ab75352495a54fc0418d1bf9111f67c2279365
chrisgerken <chris.gerken@gerkenip.com>
2015-09-25 15:30:51 -0500

    furq

--- sqoop-MEStemp/stage2.sh
+++ sqoop-MEStemp/stage2.sh
@@ -2,5 +2,6 @@ sudo cp CreateTable.txt  /home/hdfs/CreateTable.txt
 sudo cp runHive.sh  /home/hdfs/runHive.sh
 sudo cp runScoop.sh  /home/hdfs/runScoop.sh
 sudo cp wipeDB.sh  /home/hdfs/wipeDB.sh
+sudo cp hiveSimple.sh  /home/hdfs/HiveSimple.sh
 sudo cp yieldIngestWorkflow.xml  /home/hdfs/yieldIngestWorkflow.xml
 sudo cp yieldIngestProperties.properties  /home/hdfs/yieldIngestProperties.properties
