4111c67de5d5a2337cac2841b7752319a5f0db3e
chrisgerken <chris.gerken@gerkenip.com>
2015-09-28 11:20:37 -0500

    echo

--- sqoop-MEStemp/hiveSimple.sh
+++ sqoop-MEStemp/hiveSimple.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "#################################################3"
+echo "##################################################"
 echo " "
 date 
 
@@ -12,22 +12,47 @@ hadoop fs -test -d /MEStemp
                  echo "/MEStemp not present in HDFS"
     fi
 
-sqoop import --connect 'jdbc:sqlserver://172.23.236.90:1433;database=MEXICALI_OWNER' --username app_MES_ReadOnlyUserMX --password X2{01_p7gyltm --query "SELECT BuildRecordID,InvPackCreatedID,InvPackConsumedID,QuantityUsed,UnitID,BuildDT,IsEdited,RevisionNo,BuildMachineStageID, 1 as \"org_id\"  FROM dbo.BuildRecord WHERE \$CONDITIONS" --split-by BuildRecordID  --num-mappers 2 --target-dir /MEStemp/BuildRecord/1 --direct  -- --schema dbo --validate
+echo " "
+echo "##################################################"
+echo "sqoop: BuildRecord "
+echo "##################################################"
+echo " "
 
-sqoop import --connect 'jdbc:sqlserver://172.23.236.90:1433;database=MEXICALI_OWNER' --username app_MES_ReadOnlyUserMX --password X2{01_p7gyltm --query "SELECT AddressID,Address1,Address2,City,County,PostCode,Country, 1 as \"org_id\"  FROM dbo.Address WHERE \$CONDITIONS" --split-by AddressID  --num-mappers 2 --target-dir /MEStemp/Address/1 --direct  -- --schema dbo --validate
+sqoop import --connect 'jdbc:sqlserver://172.23.236.90:1433;database=MEXICALI_OWNER' --username app_MES_ReadOnlyUserMX --password X2{01_p7gyltm --query "SELECT BuildRecordID,InvPackCreatedID,InvPackConsumedID,QuantityUsed,UnitID,BuildDT,IsEdited,RevisionNo,BuildMachineStageID, 1 as \"org_id\"  FROM dbo.BuildRecord WHERE \$CONDITIONS" --split-by BuildRecordID  --num-mappers 8 --target-dir /MEStemp/BuildRecord/1 --direct  -- --schema dbo --validate
+
+echo " "
+echo "##################################################"
+echo "sqoop: Address "
+echo "##################################################"
+echo " "
+
+sqoop import --connect 'jdbc:sqlserver://172.23.236.90:1433;database=MEXICALI_OWNER' --username app_MES_ReadOnlyUserMX --password X2{01_p7gyltm --query "SELECT AddressID,Address1,Address2,City,County,PostCode,Country, 1 as \"org_id\"  FROM dbo.Address WHERE \$CONDITIONS" --num-mappers 1 --target-dir /MEStemp/Address/1 --direct  -- --schema dbo --validate
 
 
 set hive.execution.engine=tez;
+echo " "
+echo "##################################################"
+echo "Hive: Conditionally create data base MEStemp
+echo "##################################################"
+echo " "
 
-hive
+hive -e "CREATE DATABASE IF NOT EXISTS MEStemp\"
 
-CREATE DATABASE IF NOT EXISTS MEStemp;
+echo " "
+echo "##################################################"
+echo "Hive: Conditionally create table BuildRecord
+echo "##################################################"
+echo " "
 
+hive -e "Create External table IF NOT EXISTS MEStemp.BuildRecord (BuildRecordID int, InvPackCreatedID int, InvPackConsumedID int, QuantityUsed float, UnitID smallint, BuildDT timestamp, IsEdited tinyint, RevisionNo smallint, BuildMachineStageID smallint,  org_id int) row format delimited fields terminated by ',' lines terminated by '\n' stored as textfile location '/MEStemp/BuildRecord'"
 
-Create External table IF NOT EXISTS MEStemp.BuildRecord (BuildRecordID int, InvPackCreatedID int, InvPackConsumedID int, QuantityUsed float, UnitID smallint, BuildDT timestamp, IsEdited tinyint, RevisionNo smallint, BuildMachineStageID smallint,  org_id int) row format delimited fields terminated by ',' lines terminated by '\n' stored as textfile location '/MEStemp/BuildRecord/';
+echo " "
+echo "##################################################"
+echo "Hive: Conditionally create table Address
+echo "##################################################"
+echo " "
 
-
-Create External table IF NOT EXISTS MEStemp.Address (AddressID smallint, Address1 varchar(508), Address2 varchar(508), City varchar(100), County varchar(100), PostCode varchar(100), Country varchar(100),  org_id int) row format delimited fields terminated by ',' lines terminated by '\n' stored as textfile location '/MEStemp/Address/';
+hive -e "Create External table IF NOT EXISTS MEStemp.Address (AddressID smallint, Address1 varchar(508), Address2 varchar(508), City varchar(100), County varchar(100), PostCode varchar(100), Country varchar(100),  org_id int) row format delimited fields terminated by ',' lines terminated by '\n' stored as textfile location '/MEStemp/Address'"
 
 
 
