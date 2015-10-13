5e2f03aaebd9431dc1703b8579c95730078a6413
chrisgerken <chris.gerken@gerkenip.com>
2015-10-13 12:36:16 -0500

    join2

--- sqoop-MEStwo/runSqoop.sh
+++ sqoop-MEStwo/runSqoop.sh
@@ -1,12 +1,13 @@
 hdfs dfs -rm -r -skipTrash /YieldWorkflow
 
 hadoop fs -put yieldIngestWorkflow.xml /YieldWorkflow/yieldIngestWorkflow.xml 
-oozie  job  -oozie  http://prod-hubnamedaws01.sunpowercorp.com:11000/oozie  -config  yieldIngestProperties.properties  -run
- 
 hadoop fs -put yieldIngestWorkflow-SPMX.xml /YieldWorkflow/yieldIngestWorkflow-SPMX.xml
 hadoop fs -put yieldIngestWorkflow-SPMM.xml /YieldWorkflow/yieldIngestWorkflow-SPMM.xml
 hadoop fs -put yieldIngestWorkflow-SPDV.xml /YieldWorkflow/yieldIngestWorkflow-SPDV.xml
 hadoop fs -put yieldIngestWorkflow-SPTT.xml /YieldWorkflow/yieldIngestWorkflow-SPTT.xml
 hadoop fs -put yieldIngestWorkflow-SPTM.xml /YieldWorkflow/yieldIngestWorkflow-SPTM.xml
 
+
+oozie  job  -oozie  http://prod-hubnamedaws01.sunpowercorp.com:11000/oozie  -config  yieldIngestProperties.properties  -run
+ 
  
