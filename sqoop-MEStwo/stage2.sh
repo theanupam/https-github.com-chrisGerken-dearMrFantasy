eedab2f0b517944c2154e5ac244679f2a9af6aba
chrisgerken <chris.gerken@gerkenip.com>
2015-10-13 11:39:52 -0500

    works

--- sqoop-MEStwo/stage2.sh
+++ sqoop-MEStwo/stage2.sh
@@ -1,66 +1,15 @@
-sudo cp CreateTable.txt  /home/hdfs/CreateTable.txt
-sudo cp runHive.sh  /home/hdfs/runHive.sh
 sudo cp runScoop.sh  /home/hdfs/runScoop.sh
-sudo cp wipeDB.sh  /home/hdfs/wipeDB.sh
 sudo cp hiveSimple.sh  /home/hdfs/HiveSimple.sh
+
 sudo cp yieldIngestWorkflow.xml  /home/hdfs/yieldIngestWorkflow.xml
 sudo cp yieldIngestProperties.properties  /home/hdfs/yieldIngestProperties.properties
 
 
-sudo cp ingest_SPML_ModuleInfo.sh  /home/hdfs/ingest_SPML_ModuleInfo.sh
-sudo cp ingestAddress.sh  /home/hdfs/ingestAddress.sh
-sudo cp ingestCustomer.sh  /home/hdfs/ingestCustomer.sh
-sudo cp ingestDepartment.sh  /home/hdfs/ingestDepartment.sh
-sudo cp ingestInvChangeState.sh  /home/hdfs/ingestInvChangeState.sh
-sudo cp ingestInvIsolatedReason.sh  /home/hdfs/ingestInvIsolatedReason.sh
-sudo cp ingestInvState.sh  /home/hdfs/ingestInvState.sh
-sudo cp ingestLineGroup.sh  /home/hdfs/ingestLineGroup.sh
-sudo cp ingestLineInfo.sh  /home/hdfs/ingestLineInfo.sh
-sudo cp ingestLineSection.sh  /home/hdfs/ingestLineSection.sh
-sudo cp ingestLineType.sh  /home/hdfs/ingestLineType.sh
-sudo cp ingestMachineStageType.sh  /home/hdfs/ingestMachineStageType.sh
-sudo cp ingestMachineStopCategory.sh  /home/hdfs/ingestMachineStopCategory.sh
-sudo cp ingestMachineStopReason.sh  /home/hdfs/ingestMachineStopReason.sh
-sudo cp ingestOperation.sh  /home/hdfs/ingestOperation.sh
-sudo cp ingestOperationType.sh  /home/hdfs/ingestOperationType.sh
-sudo cp ingestPartClassification.sh  /home/hdfs/ingestPartClassification.sh
-sudo cp ingestPartSubClassification.sh  /home/hdfs/ingestPartSubClassification.sh
-sudo cp ingestPartType.sh  /home/hdfs/ingestPartType.sh
-sudo cp ingestScrapReason.sh  /home/hdfs/ingestScrapReason.sh
-sudo cp ingestShift.sh  /home/hdfs/ingestShift.sh
-sudo cp ingestShiftPattern.sh  /home/hdfs/ingestShiftPattern.sh
-sudo cp ingestShiftType.sh  /home/hdfs/ingestShiftType.sh
-sudo cp ingestSupplier.sh  /home/hdfs/ingestSupplier.sh
-sudo cp ingestTagList.sh  /home/hdfs/ingestTagList.sh
-sudo cp ingestUnit.sh  /home/hdfs/ingestUnit.sh
-
-sudo cp ingestBuildRecordALL.sh  /home/hdfs/ingestBuildRecordALL.sh
-
-sudo cp ingestInventoryLabelALL.sh  /home/hdfs/ingestInventoryLabelALL.sh
-
-sudo cp ingestInventoryPackALL.sh  /home/hdfs/ingestInventoryPackALL.sh
-
-sudo cp ingestJobALL.sh  /home/hdfs/ingestJobALL.sh
-
-sudo cp ingestLineALL.sh  /home/hdfs/ingestLineALL.sh
-
-sudo cp ingestLocationAll.sh  /home/hdfs/ingestLocationAll.sh
-
-sudo cp ingestMachineStageAll.sh  /home/hdfs/ingestMachineStageAll.sh
-
-sudo cp ingestMachineStopAll.sh  /home/hdfs/ingestMachineStopAll.sh
-
-sudo cp ingestPartALL.sh  /home/hdfs/ingestPartALL.sh
-
-sudo cp ingestWOALL.sh  /home/hdfs/ingestWOALL.sh
-sudo cp ingest.sh  /home/hdfs/ingest.sh
+sudo cp yieldIngestWorkflow-SPMX.xml  /home/hdfs/yieldIngestWorkflow-SPMX.xml
+sudo cp yieldIngestWorkflow-SPMM.xml  /home/hdfs/yieldIngestWorkflow-SPMM.xml
+sudo cp yieldIngestWorkflow-SPDV.xml  /home/hdfs/yieldIngestWorkflow-SPDV.xml
+sudo cp yieldIngestWorkflow-SPTT.xml  /home/hdfs/yieldIngestWorkflow-SPTT.xml
+sudo cp yieldIngestWorkflow-SPTM.xml  /home/hdfs/yieldIngestWorkflow-SPTM.xml
 
 
-sudo cp modcoIngestSPMX.sh  /home/hdfs/modcoIngestSPMX.sh
-sudo cp modcoIngestSPMM.sh  /home/hdfs/modcoIngestSPMM.sh
-sudo cp modcoIngestSPDV.sh  /home/hdfs/modcoIngestSPDV.sh
-sudo cp modcoIngestSPTT.sh  /home/hdfs/modcoIngestSPTT.sh
-sudo cp modcoIngestSPTM.sh  /home/hdfs/modcoIngestSPTM.sh
 
-sudo cp modcoIngestBefore.sh  /home/hdfs/modcoIngestBefore.sh
-sudo cp modcoIngestAfter.sh  /home/hdfs/modcoIngestAfter.sh
--- sqoop-MEStwo/xfer.bat
+++ sqoop-MEStwo/xfer.bat
@@ -1,69 +1,13 @@
-pscp  -pw d4e30f24   yieldIngestWorkflow.xml  appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/yieldIngestWorkflow.xml
-pscp  -pw d4e30f24   yieldIngestProperties.properties  appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/yieldIngestProperties.properties 
 pscp  -pw d4e30f24   runSqoop.sh        appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/runScoop.sh
-
-pscp  -pw d4e30f24   CreateTable.txt    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/CreateTable.txt
-pscp  -pw d4e30f24   runHive.sh         appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/runHive.sh
-
 pscp  -pw d4e30f24   stage2.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/stage2.sh
-pscp  -pw d4e30f24   wipeDB.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/wipeDB.sh
 pscp  -pw d4e30f24   hiveSimple.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/hiveSimple.sh
 
+pscp  -pw d4e30f24   yieldIngestWorkflow.xml  appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/yieldIngestWorkflow.xml
+pscp  -pw d4e30f24   yieldIngestProperties.properties  appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/yieldIngestProperties.properties 
 
-pscp  -pw d4e30f24   ingest/ingest_SPML_ModuleInfo.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingest_SPML_ModuleInfo.sh
-pscp  -pw d4e30f24   ingest/ingestAddress.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingestAddress.sh
-pscp  -pw d4e30f24   ingest/ingestCustomer.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingestCustomer.sh
-pscp  -pw d4e30f24   ingest/ingestDepartment.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingestDepartment.sh
-pscp  -pw d4e30f24   ingest/ingestInvChangeState.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingestInvChangeState.sh
-pscp  -pw d4e30f24   ingest/ingestInvIsolatedReason.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingestInvIsolatedReason.sh
-pscp  -pw d4e30f24   ingest/ingestInvState.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingestInvState.sh
-pscp  -pw d4e30f24   ingest/ingestLineGroup.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingestLineGroup.sh
-pscp  -pw d4e30f24   ingest/ingestLineInfo.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingestLineInfo.sh
-pscp  -pw d4e30f24   ingest/ingestLineSection.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingestLineSection.sh
-pscp  -pw d4e30f24   ingest/ingestLineType.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingestLineType.sh
-pscp  -pw d4e30f24   ingest/ingestMachineStageType.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingestMachineStageType.sh
-pscp  -pw d4e30f24   ingest/ingestMachineStopCategory.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingestMachineStopCategory.sh
-pscp  -pw d4e30f24   ingest/ingestMachineStopReason.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingestMachineStopReason.sh
-pscp  -pw d4e30f24   ingest/ingestOperation.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingestOperation.sh
-pscp  -pw d4e30f24   ingest/ingestOperationType.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingestOperationType.sh
-pscp  -pw d4e30f24   ingest/ingestPartClassification.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingestPartClassification.sh
-pscp  -pw d4e30f24   ingest/ingestPartSubClassification.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingestPartSubClassification.sh
-pscp  -pw d4e30f24   ingest/ingestPartType.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingestPartType.sh
-pscp  -pw d4e30f24   ingest/ingestScrapReason.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingestScrapReason.sh
-pscp  -pw d4e30f24   ingest/ingestShift.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingestShift.sh
-pscp  -pw d4e30f24   ingest/ingestShiftPattern.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingestShiftPattern.sh
-pscp  -pw d4e30f24   ingest/ingestShiftType.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingestShiftType.sh
-pscp  -pw d4e30f24   ingest/ingestSupplier.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingestSupplier.sh
-pscp  -pw d4e30f24   ingest/ingestTagList.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingestTagList.sh
-pscp  -pw d4e30f24   ingest/ingestUnit.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingestUnit.sh
+pscp  -pw d4e30f24   yieldIngestWorkflow-SPMX.xml    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/yieldIngestWorkflow-SPMX.xml
+pscp  -pw d4e30f24   yieldIngestWorkflow-SPMM.xml    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/yieldIngestWorkflow-SPMM.xml
+pscp  -pw d4e30f24   yieldIngestWorkflow-SPDV.xml    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/yieldIngestWorkflow-SPDV.xml
+pscp  -pw d4e30f24   yieldIngestWorkflow-SPTT.xml    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/yieldIngestWorkflow-SPTT.xml
+pscp  -pw d4e30f24   yieldIngestWorkflow-SPTM.xml    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/yieldIngestWorkflow-SPTM.xml
 
-pscp  -pw d4e30f24   ingest/ingestBuildRecordALL.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingestBuildRecordALL.sh
-
-pscp  -pw d4e30f24   ingest/ingestInventoryLabelALL.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingestInventoryLabelALL.sh
-
-pscp  -pw d4e30f24   ingest/ingestInventoryPackALL.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingestInventoryPackALL.sh
-
-pscp  -pw d4e30f24   ingest/ingestJobALL.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingestJobALL.sh
-
-pscp  -pw d4e30f24   ingest/ingestLineALL.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingestLineALL.sh
-
-pscp  -pw d4e30f24   ingest/ingestLocationAll.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingestLocationAll.sh
-
-pscp  -pw d4e30f24   ingest/ingestMachineStageAll.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingestMachineStageAll.sh
-
-pscp  -pw d4e30f24   ingest/ingestMachineStopAll.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingestMachineStopAll.sh
-
-pscp  -pw d4e30f24   ingest/ingestPartALL.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingestPartALL.sh
-
-pscp  -pw d4e30f24   ingest/ingestWOALL.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingestWOALL.sh
-pscp  -pw d4e30f24   ingest/ingest.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/ingest.sh
-
-
-pscp  -pw d4e30f24   ingest/modcoIngestSPMX.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/modcoIngestSPMX.sh
-pscp  -pw d4e30f24   ingest/modcoIngestSPMM.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/modcoIngestSPMM.sh
-pscp  -pw d4e30f24   ingest/modcoIngestSPDV.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/modcoIngestSPDV.sh
-pscp  -pw d4e30f24   ingest/modcoIngestSPTT.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/modcoIngestSPTT.sh
-pscp  -pw d4e30f24   ingest/modcoIngestSPTM.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/modcoIngestSPTM.sh
-
-pscp  -pw d4e30f24   ingest/modcoIngestBefore.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/modcoIngestBefore.sh
-pscp  -pw d4e30f24   ingest/modcoIngestAfter.sh    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/modcoIngestAfter.sh
