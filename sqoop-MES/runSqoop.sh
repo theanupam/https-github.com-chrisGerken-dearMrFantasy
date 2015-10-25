62878f27da2fa5a27957e4896405bb2bbf431963
chrisgerken <chris.gerken@gerkenip.com>
2015-10-25 17:12:25 -0500

    3 long

--- sqoop-MES/runSqoop.sh
+++ sqoop-MES/runSqoop.sh
@@ -2,7 +2,9 @@ hdfs dfs -rm -r -skipTrash /YieldWorkflow
 hdfs dfs -mkdir /YieldWorkflow
 
 hadoop fs -put yieldIngestWorkflow.xml /YieldWorkflow/yieldIngestWorkflow.xml 
-hadoop fs -put yieldIngestWorkflow-Long.xml /YieldWorkflow/yieldIngestWorkflow-Long.xml
+hadoop fs -put yieldIngestWorkflow-Long-0.xml /YieldWorkflow/yieldIngestWorkflow-Long-0.xml
+hadoop fs -put yieldIngestWorkflow-Long-1.xml /YieldWorkflow/yieldIngestWorkflow-Long-1.xml
+hadoop fs -put yieldIngestWorkflow-Long-2.xml /YieldWorkflow/yieldIngestWorkflow-Long-2.xml
 hadoop fs -put yieldIngestWorkflow-Short-0.xml /YieldWorkflow/yieldIngestWorkflow-Short-0.xml
 hadoop fs -put yieldIngestWorkflow-Short-1.xml /YieldWorkflow/yieldIngestWorkflow-Short-1.xml
 hadoop fs -put yieldIngestWorkflow-Short-2.xml /YieldWorkflow/yieldIngestWorkflow-Short-2.xml
--- sqoop-MES/stage2.sh
+++ sqoop-MES/stage2.sh
@@ -5,7 +5,9 @@ sudo cp yieldIngestWorkflow.xml  /home/hdfs/yieldIngestWorkflow.xml
 sudo cp yieldIngestProperties.properties  /home/hdfs/yieldIngestProperties.properties
 
 
-sudo cp yieldIngestWorkflow-Long.xml  /home/hdfs/yieldIngestWorkflow-Long.xml
+sudo cp yieldIngestWorkflow-Long-0.xml  /home/hdfs/yieldIngestWorkflow-Long-0.xml
+sudo cp yieldIngestWorkflow-Long-1.xml  /home/hdfs/yieldIngestWorkflow-Long-1.xml
+sudo cp yieldIngestWorkflow-Long-2.xml  /home/hdfs/yieldIngestWorkflow-Long-2.xml
 sudo cp yieldIngestWorkflow-Short-0.xml  /home/hdfs/yieldIngestWorkflow-Short-0.xml
 sudo cp yieldIngestWorkflow-Short-1.xml  /home/hdfs/yieldIngestWorkflow-Short-1.xml
 sudo cp yieldIngestWorkflow-Short-2.xml  /home/hdfs/yieldIngestWorkflow-Short-2.xml
