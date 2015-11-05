717e7497615dd43be4cd438e0c34cfb0ca6fb348
chrisgerken <chris.gerken@gerkenip.com>
2015-11-05 12:08:59 -0600

    fx 2

--- sqoop-MES/ModcoIngest/runSqoop.sh
+++ sqoop-MES/ModcoIngest/runSqoop.sh
@@ -8,6 +8,13 @@ hadoop fs -put yieldIngestWorkflow-Full-2.xml /IncrementalYield/yieldIngestWorkf
 hadoop fs -put yieldIngestWorkflow-Incr-0.xml /IncrementalYield/yieldIngestWorkflow-Incr-0.xml
 hadoop fs -put yieldIngestWorkflow-Incr-1.xml /IncrementalYield/yieldIngestWorkflow-Incr-1.xml
 
+oozie  validate  yieldIngestWorkflow-Full-0.xml  
+oozie  validate  yieldIngestWorkflow-Full-1.xml  
+oozie  validate  yieldIngestWorkflow-Full-2.xml  
+oozie  validate  yieldIngestWorkflow-Incr-0.xml  
+oozie  validate  yieldIngestWorkflow-Incr-1.xml  
+
+oozie  validate  yieldIngestWorkflow.xml
 
 oozie  job  -oozie  http://prod-hubnamedaws01.sunpowercorp.com:11000/oozie  -config  yieldIngestProperties.properties  -run
  
