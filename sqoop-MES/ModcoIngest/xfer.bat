bac5c350e5dfee77bf4ab87e8e9c1eef53e1b20c
chrisgerken <chris.gerken@gerkenip.com>
2015-11-05 20:33:12 -0600

    2 by 6

--- sqoop-MES/ModcoIngest/xfer.bat
+++ sqoop-MES/ModcoIngest/xfer.bat
@@ -12,6 +12,5 @@ pscp  -pw d4e30f24   yieldIngestWorkflow-Full-1.xml    appladm@prod-hubnamedaws0
 pscp  -pw d4e30f24   yieldIngestWorkflow-Full-2.xml    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/yieldIngestWorkflow-Full-2.xml
 pscp  -pw d4e30f24   yieldIngestWorkflow-Incr-0.xml    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/yieldIngestWorkflow-Incr-0.xml
 pscp  -pw d4e30f24   yieldIngestWorkflow-Incr-1.xml    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/yieldIngestWorkflow-Incr-1.xml
-pscp  -pw d4e30f24   yieldIngestWorkflow-Incr-2.xml    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/yieldIngestWorkflow-Incr-2.xml
 
 
