e7ce8b7258db3c7e6a0e61d6fae71c1802078a8e
chrisgerken <chris.gerken@gerkenip.com>
2015-11-05 22:31:13 -0600

    2 and 2

--- sqoop-MES/ModcoIngest/runSqoop.sh
+++ sqoop-MES/ModcoIngest/runSqoop.sh
@@ -4,13 +4,11 @@ hdfs dfs -mkdir /IncrementalYield
 hadoop fs -put yieldIngestWorkflow.xml /IncrementalYield/yieldIngestWorkflow.xml 
 hadoop fs -put yieldIngestWorkflow-Full-0.xml /IncrementalYield/yieldIngestWorkflow-Full-0.xml
 hadoop fs -put yieldIngestWorkflow-Full-1.xml /IncrementalYield/yieldIngestWorkflow-Full-1.xml
-hadoop fs -put yieldIngestWorkflow-Full-2.xml /IncrementalYield/yieldIngestWorkflow-Full-2.xml
 hadoop fs -put yieldIngestWorkflow-Incr-0.xml /IncrementalYield/yieldIngestWorkflow-Incr-0.xml
 hadoop fs -put yieldIngestWorkflow-Incr-1.xml /IncrementalYield/yieldIngestWorkflow-Incr-1.xml
 
 oozie  validate  yieldIngestWorkflow-Full-0.xml  
 oozie  validate  yieldIngestWorkflow-Full-1.xml  
-oozie  validate  yieldIngestWorkflow-Full-2.xml  
 oozie  validate  yieldIngestWorkflow-Incr-0.xml  
 oozie  validate  yieldIngestWorkflow-Incr-1.xml  
 
--- sqoop-MES/ModcoIngest/xfer.bat
+++ sqoop-MES/ModcoIngest/xfer.bat
@@ -9,7 +9,6 @@ pscp  -pw d4e30f24   com.intersys.sqoop.driver.jar  appladm@prod-hubnamedaws01.s
 
 pscp  -pw d4e30f24   yieldIngestWorkflow-Full-0.xml    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/yieldIngestWorkflow-Full-0.xml
 pscp  -pw d4e30f24   yieldIngestWorkflow-Full-1.xml    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/yieldIngestWorkflow-Full-1.xml
-pscp  -pw d4e30f24   yieldIngestWorkflow-Full-2.xml    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/yieldIngestWorkflow-Full-2.xml
 pscp  -pw d4e30f24   yieldIngestWorkflow-Incr-0.xml    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/yieldIngestWorkflow-Incr-0.xml
 pscp  -pw d4e30f24   yieldIngestWorkflow-Incr-1.xml    appladm@prod-hubnamedaws01.sunpowercorp.com:/home/appladm/yieldIngestWorkflow-Incr-1.xml
 
