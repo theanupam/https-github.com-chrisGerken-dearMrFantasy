bbd3794795b754bef744d2a51b56482cb70c60a6
chrisgerken <chris.gerken@gerkenip.com>
2015-11-13 15:00:29 -0600

    finer

--- sqoop-MES/ModcoIngest/runSqoop.sh
+++ sqoop-MES/ModcoIngest/runSqoop.sh
@@ -2,22 +2,38 @@ hdfs dfs -rm -r -skipTrash /IncrementalYield
 hdfs dfs -mkdir /IncrementalYield
 
 hadoop fs -put yieldIngestWorkflow.xml /IncrementalYield/yieldIngestWorkflow.xml 
-hadoop fs -put yieldIngestWorkflow-Full-0.xml /IncrementalYield/yieldIngestWorkflow-Full-0.xml
-hadoop fs -put yieldIngestWorkflow-Full-1.xml /IncrementalYield/yieldIngestWorkflow-Full-1.xml
 hadoop fs -put yieldIngestWorkflow-Incr-0.xml /IncrementalYield/yieldIngestWorkflow-Incr-0.xml
 hadoop fs -put yieldIngestWorkflow-Incr-1.xml /IncrementalYield/yieldIngestWorkflow-Incr-1.xml
+hadoop fs -put yieldIngestWorkflow-Incr-2.xml /IncrementalYield/yieldIngestWorkflow-Incr-2.xml
+hadoop fs -put yieldIngestWorkflow-Incr-3.xml /IncrementalYield/yieldIngestWorkflow-Incr-3.xml
+hadoop fs -put yieldIngestWorkflow-Full-0.xml /IncrementalYield/yieldIngestWorkflow-Full-0.xml
+hadoop fs -put yieldIngestWorkflow-Full-1.xml /IncrementalYield/yieldIngestWorkflow-Full-1.xml
+hadoop fs -put yieldIngestWorkflow-Full-2.xml /IncrementalYield/yieldIngestWorkflow-Full-2.xml
+hadoop fs -put yieldIngestWorkflow-Full-3.xml /IncrementalYield/yieldIngestWorkflow-Full-3.xml
 
+echo "Validating yieldIngestWorkflow-Incr-0.xml";
+oozie  validate  yieldIngestWorkflow-Incr-0.xml  
+echo "";
+echo "Validating yieldIngestWorkflow-Incr-1.xml";
+oozie  validate  yieldIngestWorkflow-Incr-1.xml  
+echo "";
+echo "Validating yieldIngestWorkflow-Incr-2.xml";
+oozie  validate  yieldIngestWorkflow-Incr-2.xml  
+echo "";
+echo "Validating yieldIngestWorkflow-Incr-3.xml";
+oozie  validate  yieldIngestWorkflow-Incr-3.xml  
+echo "";
 echo "Validating yieldIngestWorkflow-Full-0.xml";
 oozie  validate  yieldIngestWorkflow-Full-0.xml  
 echo "";
 echo "Validating yieldIngestWorkflow-Full-1.xml";
 oozie  validate  yieldIngestWorkflow-Full-1.xml  
 echo "";
-echo "Validating yieldIngestWorkflow-Incr-0.xml";
-oozie  validate  yieldIngestWorkflow-Incr-0.xml  
+echo "Validating yieldIngestWorkflow-Full-2.xml";
+oozie  validate  yieldIngestWorkflow-Full-2.xml  
 echo "";
-echo "Validating yieldIngestWorkflow-Incr-1.xml";
-oozie  validate  yieldIngestWorkflow-Incr-1.xml  
+echo "Validating yieldIngestWorkflow-Full-3.xml";
+oozie  validate  yieldIngestWorkflow-Full-3.xml  
 echo "";
 
 echo "Validating yieldIngestWorkflow.xml";
